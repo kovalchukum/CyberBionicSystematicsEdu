f9dad0c118c8d7eea0276ffdf800397376a9589e
y.kovalchuk <y.kovalchuk@infoplus.com.ua>
2019-05-08 11:50:45 +0300

    first commit

---
+++ 01.Collections/Task.02/Task.02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task._02
+{
+    //Создайте коллекцию, в которой бы хранились наименования 12 месяцев, порядковый номер и
+    //количество дней в соответствующем месяце.Реализуйте возможность выбора месяцев, как по
+    //порядковому номеру, так и количеству дней в месяце, при этом результатом может быть не
+    //только один месяц.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
